dfa65a66a4f14c217a046d8cccf0318f692bb89a
Matteo Anselmo <matteo.anselmo96@gmail.com>
2022-05-15 12:21:04 +0200

    implementazione index-card con controllo su cancellazione treno

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -1,12 +1,17 @@
 <?php
-
+use App\Train;
 namespace App\Http\Controllers;
 
+use App\Train;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index(){
-        return view('trains.index');
+        $trains = Train::all();
+        $data = [
+            'trains' => $trains,
+        ];
+        return view('trains.index', $data);
     }
 }
--- resources/views/trains/index.blade.php
+++ resources/views/trains/index.blade.php
@@ -6,6 +6,20 @@
             <div class="row">
                 <div class="col-12">
                     <h1>List of trains </h1>
+                    @foreach ($trains as $train)
+                        @if (!$train->deleted)
+                            <div class="col-3">
+                                <div class="card mb-3 p-3 text-dark" style="width: 18rem;">
+                                    <div class="card-body">
+                                        <h5 class="card-title">{{$train->agency}}</h5>
+                                        <h6 class="card-subtitle mb-2 ">il treno in partenza da: {{$train->departure_station}}, delle ore:  {{$train->departure_time}}</h6>
+                                        <h6 class="card-subtitle mb-2 ">in arrivo a: {{$train->arrival_station}}, alle ore: {{$train->arrival_time}}</h6>
+                                        <p class="card-text">codice treno: {{$train->train_code}}, numero di carrozze: {{$train->number_of_carriages}}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        @endif
+                    @endforeach
                 </div>
             </div>
         </div>
